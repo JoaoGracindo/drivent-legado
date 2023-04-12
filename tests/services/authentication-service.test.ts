38ffbe8f6682471ec001e88e405eed586695788b
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-04-11 21:43:03 -0300

    Pull from T9 repo

--- tests/services/authentication-service.test.ts
+++ tests/services/authentication-service.test.ts
@@ -1,13 +1,8 @@
 import faker from '@faker-js/faker';
 import { createUser } from '../factories';
 import { cleanDb } from '../helpers';
-<<<<<<< HEAD
-import { prisma } from '@/config';
-import { init, close } from '@/app';
-=======
 import { init } from '@/app';
 import { prisma } from '@/config';
->>>>>>> 72ab463d0e240c1b816829a0566b4e4b2b461875
 import authenticationService, { invalidCredentialsError } from '@/services/authentication-service';
 
 beforeAll(async () => {
@@ -15,13 +10,6 @@ beforeAll(async () => {
   await cleanDb();
 });
 
-<<<<<<< HEAD
-afterAll(async () => {
-  await close();
-});
-
-=======
->>>>>>> 72ab463d0e240c1b816829a0566b4e4b2b461875
 describe('signIn', () => {
   const generateParams = () => ({
     email: faker.internet.email(),
